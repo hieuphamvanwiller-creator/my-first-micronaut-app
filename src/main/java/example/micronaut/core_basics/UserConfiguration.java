a9d55e31e906b61739cdf62f7274821e423204d0
hieu.phamvan.willer <hieu.phamvan.willer@vti.com.vn>
2025-10-14 11:50:03 +0100

    feat: @ConfigurationProperties annotation

---
+++ src/main/java/example/micronaut/core_basics/UserConfiguration.java
@@ -0,0 +1,34 @@
+package example.micronaut.core_basics;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+
+@ConfigurationProperties("user")
+public class UserConfiguration {
+    private String username;
+    private String fullName;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/test/java/example/micronaut/UserConfigurationTest.java
@@ -0,0 +1,30 @@
+package example.micronaut;
+
+import example.micronaut.core_basics.UserConfiguration;
+import io.micronaut.context.ApplicationContext;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserConfigurationTest {
+
+    @Test
+    void testTeamConfiguration() {
+//        Map<String, Object> items = new HashMap<>();
+//        items.put("user.username", "user001");
+//        items.put("user.email", "user001@gmail.com");
+//        items.put("user.full-name", "USER 001");
+
+        ApplicationContext ctx = ApplicationContext.run();
+        UserConfiguration userConfiguration = ctx.getBean(UserConfiguration.class);
+
+        assertEquals("user001", userConfiguration.getUsername());
+        assertEquals("USER 001", userConfiguration.getFullName());
+        assertEquals("user001@gmail.com", userConfiguration.getEmail());
+
+        ctx.close();
+    }
+}
